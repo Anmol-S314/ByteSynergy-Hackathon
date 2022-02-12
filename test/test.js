0e0489989ed95a1e6e5126a53e6c1cc517f751d1
Abhinav-Mahajan10 <77499650+Abhinav-Mahajan10@users.noreply.github.com>
2022-02-12 17:42:16 +0530

    Add files via upload

---
+++ test/test.js
@@ -0,0 +1,90 @@
+const TumblrCloneDapp = artifacts.require('./TumblrCloneDapp.sol')
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
+contract('TumblrCloneDapp', ([deployer, author, tipper]) => {
+  let app
+
+  before(async () => {
+    app = await TumblrCloneDapp.deployed()
+  })
+
+  describe('deployment', async () => {
+    it('deploys successfully', async () => {
+      const address = await app.address
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address, '')
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
+    })
+
+    it('has a name', async () => {
+      const name = await app.name()
+      assert.equal(name, 'TumblrCloneDapp')
+    })
+  })
+
+  describe('images', async () => {
+    let result, imageCount
+    const hash = 'QmV8cfu6n4NT5xRr2AHdKxFMTZEJrA44qgrBCr739BN9Wb'
+
+    before(async () => {
+      result = await app.uploadImage(hash, 'Image description', { from: author })
+      imageCount = await app.imageCount()
+    })
+
+    it('creates images', async () => {
+      assert.equal(imageCount, 1)
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(), imageCount.toNumber(), 'id is correct')
+      assert.equal(event.hash, hash, 'Hash is correct')
+      assert.equal(event.description, 'Image description', 'description is correct')
+      assert.equal(event.tipAmount, '0', 'tip amount is correct')
+      assert.equal(event.author, author, 'author is correct')
+
+      await app.uploadImage('', 'Image description', { from: author }).should.be.rejected;
+
+      await app.uploadImage('Image hash', '', { from: author }).should.be.rejected;
+    })
+
+    it('lists images', async () => {
+      const image = await app.images(imageCount)
+      assert.equal(image.id.toNumber(), imageCount.toNumber(), 'id is correct')
+      assert.equal(image.hash, hash, 'Hash is correct')
+      assert.equal(image.description, 'Image description', 'description is correct')
+      assert.equal(image.tipAmount, '0', 'tip amount is correct')
+      assert.equal(image.author, author, 'author is correct')
+    })
+
+    it('allows users to tip images', async () => {
+      let oldAuthorBalance
+      oldAuthorBalance = await web3.eth.getBalance(author)
+      oldAuthorBalance = new web3.utils.BN(oldAuthorBalance)
+
+      result = await app.tipImageOwner(imageCount, { from: tipper, value: web3.utils.toWei('1', 'Ether') })
+
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(), imageCount.toNumber(), 'id is correct')
+      assert.equal(event.hash, hash, 'Hash is correct')
+      assert.equal(event.description, 'Image description', 'description is correct')
+      assert.equal(event.tipAmount, '1000000000000000000', 'tip amount is correct')
+      assert.equal(event.author, author, 'author is correct')
+
+      let newAuthorBalance
+      newAuthorBalance = await web3.eth.getBalance(author)
+      newAuthorBalance = new web3.utils.BN(newAuthorBalance)
+
+      let tipImageOwner
+      tipImageOwner = web3.utils.toWei('1', 'Ether')
+      tipImageOwner = new web3.utils.BN(tipImageOwner)
+
+      const expectedBalance = oldAuthorBalance.add(tipImageOwner)
+
+      assert.equal(newAuthorBalance.toString(), expectedBalance.toString())
+
+      await app.tipImageOwner(99, { from: tipper, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+    })
+  })
+})
