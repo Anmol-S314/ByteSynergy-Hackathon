a7350ca4f51deae5146b7e6fcb4855fd18b96c4a
aamodbk <77502673+aamodbk@users.noreply.github.com>
2022-02-14 00:54:57 +0530

    added

--- truffle-config.js
+++ truffle-config.js
@@ -9,13 +9,7 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*", // Match any network id
-      //gas: 50000000,
-      //gasLimit: 8000000
-      //host: 'localhost',
-      //port: 8545,
-      //network_id: '*',
-      //gas: 0x1fffffffffffff
+      network_id: "*" // Match any network id
     },
     ropsten: {
       provider: function() {
@@ -33,7 +27,6 @@ module.exports = {
   contracts_build_directory: './src/abis/',
   compilers: {
     solc: {
-      version: "^0.5.0",
       optimizer: {
         enabled: true,
         runs: 200
